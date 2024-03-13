9a56f4671247ed1420bc072e82d6ff8719704e92
mshendrikx <mauricio.servatius@gmail.com>
2024-03-13 10:03:29 -0300

    main

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, jsonify
+import time
+
+WAIT_VIDEO = "static/videos/karacool_wait.mp4"
+
+app = Flask(__name__)
+
+# List of video URLs
+video_urls = ["static/songs/" + video for video in 
+              ["Bon Jovi - Livin` On A Prayer.mp4", 
+              "Pearl Jam - State of Love and Trust.mp4",
+              "Chrystian e Ralf - Saudade.mp4"]]
+
+@app.route("/")
+def index():
+  next_url = WAIT_VIDEO
+  return render_template("index.html", next_video_url=next_url)
+
+@app.route("/player")
+def player():
+  next_url = WAIT_VIDEO
+  return render_template("player.html", next_video_url=next_url)
+
+@app.route("/next-video")
+def get_next_song():
+  try:
+    next_url = video_urls[0]
+    del video_urls[0]
+  except:
+    next_url = WAIT_VIDEO
+  return jsonify({"url": next_url})
+
+if __name__ == "__main__":
+  app.run(debug=True, port=7001)
