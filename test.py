249bbb1f24f4f85f7fa2c6d99b56569e4370a8c9
Mauricio Servatius Hendrikx <mauricio.servatius@gmail.com>
2024-05-02 13:49:04 -0300

    main

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 from mailjet_rest import Client
 import os
 
-api_key = 'c847b4f2ee15b0ac5ae31d7d0943e4d0'
-api_secret = '23bffef98a23819a8e50a040aacd1092'
 
-#api_key = '49421fab95c47aac60a502b82cb36e3a'
-#api_secret = '504c74f9068d61b20e8d910b15222e49'
 
 mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 data = {
