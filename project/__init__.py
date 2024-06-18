7c51a6356ebdab5f61511baa77141e44a4d9d1f2
Mauricio Servatius Hendrikx <mauricio.servatius@gmail.com>
2024-06-18 14:23:36 -0300

    main

--- project/__init__.py
+++ project/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from werkzeug.security import generate_password_hash
-from flask_babel import Babel
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
