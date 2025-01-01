bebaf5450409df6593bcbb0a6445dc1a025e4de5
Maurício Servatius Hendrikx <mauricio.servatius@gmail.com>
2025-01-01 12:37:20 +0000

    main

--- karatubed.py
+++ karatubed.py
@@ -7,7 +7,6 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pytubefix import YouTube
-from dotenv import load_dotenv
 from pathlib import Path
 
 YT_BASE_URL = "https://www.youtube.com/watch?v="
@@ -45,17 +44,11 @@ def is_script_running():
             return True
     return False
 
-load_dotenv()
-
-#if is_script_running():
-#    sys.exit()
-
 session = get_session()
 
 songs = session.query(Song).filter_by(downloaded=0)
 
 for song in songs:
-#    rm_cmd = ["rm"]
     video_file = str(song.youtubeid) + ".mp4"
     filename = os.environ.get("LOCAL_PATH") + "/" + video_file
     if not os.path.exists(video_file):
@@ -64,12 +57,3 @@ for song in songs:
             YouTube(download_url).streams.first().download(filename=filename)
         except Exception as e:
            continue
-
-    rsync_cmd = ["rsync", "-avz"]    
-    rsync_cmd.extend([filename, os.environ.get("REMOTE_PATH")])
-    try:
-        result = subprocess.run(rsync_cmd, check=True, capture_output=True, text=True)
-        song.downloaded = 1
-        session.commit()
-    except subprocess.CalledProcessError as e:
-        1 == 1
